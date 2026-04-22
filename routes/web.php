e1c9f04ec834114958840957d53401ca9e282773
fachrizarizky <fachriza.rizky21@gmail.com>
2026-04-22 10:58:34 +0700

    tugas pertemuan 7

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,39 @@ Route::get('pert5', function () {
 Route::get('dosen', [DosenController::class, 'index']);
 
 Route::get('biodata', [DosenController::class, 'biodata']);
+
+Route::get('pert1', function () {
+    return view('pertemuan1');
+});
+
+Route::get('pert2part1', function () {
+    return view('pertemuan2part1');
+});
+
+Route::get('pert2part2', function () {
+    return view('pertemuan2part2');
+});
+
+Route::get('pert3part1', function () {
+    return view('pertemuan3part1');
+});
+
+Route::get('pert3part2', function () {
+    return view('pertemuan3part2');
+});
+
+Route::get('tugaspert3', function () {
+    return view('tugasPertemuan3');
+});
+
+Route::get('pert4', function () {
+    return view('pertemuan4');
+});
+
+Route::get('pert5part2', function () {
+    return view('pertemuan5part2');
+});
+
+Route::get('tugaspert5', function () {
+    return view('tugasPertemuan5');
+});
